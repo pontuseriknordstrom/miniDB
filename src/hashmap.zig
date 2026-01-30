6f0ac022628b8cb8a2b65504518ddb990696a77e
pontus <ponord@ug.kth.se>
2026-01-30 17:40:36 +0100

    feat: Implemented the djb2 algorithm for hashing

--- src/hashmap.zig
+++ src/hashmap.zig
@@ -2,14 +2,14 @@
 // http://www.cse.yorku.ca/~oz/hash.html
 //
 // NOTE: In Zig a string doesn't have a special type but are just byte slices.
-fn djb2(key: []const u8) u32 {
+pub fn djb2(key: []const u8) u32 {
     var hash: u32 = 5381;
     var c: u8 = 0;
     var i: usize = 0;
 
-    while (i < key.len()) : (i += 1) {
+    while (i < key.len) : (i += 1) {
         c = key[i];
-        hash = ((hash << 5) + hash) + c;
+        hash = ((hash << 5) +% hash) + c;
     }
 
     return hash;
